8f7c6c339e12e9b6d9147b57dc63303e2bbf1e06
UdanaIsiwari <issiwari@gmail.com>
2022-11-02 13:12:14 +0530

    update traveller and delete traveller backend completed

--- BACKEND/routes/travellers.js
+++ BACKEND/routes/travellers.js
@@ -115,13 +115,6 @@ router.route("/addTraveller/:type").post(async(req,res)=>{
     }else{
         console.log("cannot add traveller...",error);
     }
-
-    // newTraveller.save().then(()=>{
-    //     res.json("traveller added")
-    // }).catch((err)=>{
-    //     console.log(err);
-    // })
-
 })
 
 //get a traveller
@@ -151,11 +144,13 @@ router.route("/getTraveller/:type/:id").get(async(req,res)=>{
 
 })
 
-//get travellerts
+//get all travellerts
 router.route("/getTravellers/:type").get(async(req,res)=>{
     let travellerType = req.params.type;
+    console.log("get travellers");
 
     if(travellerType == "Local Traveller"){
+        console.log("get local travellers");
 
         LocalTraveller.find().then((travellers)=>{
             res.json(travellers)
@@ -164,6 +159,7 @@ router.route("/getTravellers/:type").get(async(req,res)=>{
         })
 
     }else if(travellerType == "Foreign Traveller"){
+        console.log("get foreign travellers");
 
         ForeignTraveller.find().then((travellers)=>{
             res.json(travellers)
@@ -172,5 +168,79 @@ router.route("/getTravellers/:type").get(async(req,res)=>{
         })
     }
 })
+
+//update traveller
+router.route("/updateTraveller/:type/:id").put(async(req,res)=>{
+    let travellerID = req.params.id;
+    let travellerType = req.params.type;
+    
+    console.log("update traveller", travellerType, req);
+
+    if(travellerType == "LocalTraveller"){
+        console.log("update local traveller");
+        const{Name,Address,Phone,Email,NIC} = req.body;
+
+        const updateLocalTraveller = {
+            Name,
+            Address,
+            Phone,
+            Email,
+            NIC
+        }
+
+        await LocalTraveller.findByIdAndUpdate(travellerID,updateLocalTraveller).then((traveller)=>{
+            res.json(traveller)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }else if(travellerType == "ForeignTraveller"){
+        console.log("update foreign traveller");
+        const{Name,Phone,Email,PassportNo} = req.body;
+
+        const updateForeignTraveller = {
+            Name,
+            Phone,
+            Email,
+            PassportNo
+        }
+
+        await ForeignTraveller.findByIdAndUpdate(travellerID,updateForeignTraveller).then((traveller)=>{
+            res.json(traveller)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+
+})
+
+//delete traveller
+router.route("/deleteTraveller/:type/:id").delete(async(req,res)=>{
+    let TravellerId = req.params.id;
+    let travellerType = req.params.type;
+
+    console.log("delete traveller");
+    if(travellerType == "LocalTraveller"){
+
+        console.log("delete local  traveller");
+
+        await LocalTraveller.findByIdAndDelete(TravellerId).then((traveller)=>{
+            res.json(traveller)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }else if(travellerType == "ForeignTraveller"){
+
+        console.log("delete foreign  traveller");
+
+        await ForeignTraveller.findByIdAndDelete(TravellerId).then((traveller)=>{
+            res.json(traveller)
+            console.log("delete F");
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+})
+
+
 
 module.exports =router;
